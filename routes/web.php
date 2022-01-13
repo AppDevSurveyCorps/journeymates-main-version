3c220f5191e4408cc4dcb8a2daebbe2894e64cdd
Ananda Riza <rizapratomo@graduate.utm.my>
2022-01-13 12:56:45 +0700

    update routing in web php

--- routes/web.php
+++ routes/web.php
@@ -33,13 +33,15 @@ Route::get('/signout','App\Http\Controllers\UserController@logout');
 
 Route::get('dashboard','App\Http\Controllers\UserController@dashboard'); 
 
-Route::get('profile', [UserController::class, 'profile']);
-
-Route::get('bookmarks', [UserController::class, 'bookmarks']);
-
 Route::get('reviewhome', [UserController::class, 'reviewhome']);
 
+
 Route::group(['middleware' => 'auth'], function(){
+    Route::get('profile', [UserController::class, 'profile']);
+
+    Route::get('bookmarks', [UserController::class, 'bookmarks']);
+
+    
 
 
 });
