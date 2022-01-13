6666c2683dafdea6c47a1ce423381f46cfe5e53c
JambulJambul <hilman_fahmi21@yahoo.com>
2022-01-13 14:46:10 +0700

    auto uppercase

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ auth()->user()->fname }} - JourneyMates</title>
+    <title>{{ucwords(auth()->user()->fname)}} - JourneyMates</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -90,7 +90,7 @@
                                 </div>
                                 <div class="text-center mt-12">
                                     <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-                                        {{ auth()->user()->fname }}
+                                        {{ucwords(auth()->user()->fname)}}
                                     </h3>
                                     <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
                                         <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
