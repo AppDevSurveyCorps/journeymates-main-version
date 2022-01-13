587abd9289f859f9585915cf06b34230bd66f208
JambulJambul <hilman_fahmi21@yahoo.com>
2022-01-13 14:49:03 +0700

    bookmark button update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -53,16 +53,20 @@
                                     </div>
                                     <div class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                                         <div class="py-6 px-3 mt-32 sm:mt-0">
+                                            <a href="#">
                                             <button
                                                 class="bg-emerald-400 active:bg-emerald-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                                                 type="button">
                                                 Edit profile
                                             </button>
+                                            </a>
+                                            <a href="{{ url('bookmarks') }}">
                                             <button
                                                 class="bg-emerald-400 active:bg-emerald-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                                                 type="button">
                                                 Bookmarks
                                             </button>
+                                            </a>
                                         </div>
                                     </div>
 
