f6973b7a398a09a1fb477f49dae5ebca1340dc09
Ananda Riza <rizapratomo@graduate.utm.my>
2022-01-21 14:21:27 +0700

    Update admin_dashboard.blade.php

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -64,7 +64,7 @@
                             class="mt-3 py-2 text-sm text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-100  hover:bg-gray-200 dark:hover:bg-gray-800 rounded">Manage users</a>
                         
                         <a href="{{ url('manage_place') }}"
-                            class="mt-3 py-2 text-sm text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-100  hover:bg-gray-200 dark:hover:bg-gray-800 rounded">Manage users</a>
+                            class="mt-3 py-2 text-sm text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-100  hover:bg-gray-200 dark:hover:bg-gray-800 rounded">Manage Palaces</a>
 
                     </nav>
                 </div>
